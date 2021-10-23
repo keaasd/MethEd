520ba12940b1f8950601f87103c2ff11d0b4c2e4
Евгений Коротин <kea-asd@yandex.ru>
2021-10-23 20:52:45 +0700

    v

---
+++ potrfolio/js/index.js
@@ -0,0 +1,22 @@
+new Swiper(".swiper", {
+    navigation: {
+        nextEl: '.portfolio__arrow_right',
+        prevEl: '.portfolio__arrow_left', 
+        disabledClass: ".portfolio__arrow_disable",
+    },
+    breakpointsBase: {
+        480: {
+            grid: {
+                fill: 'row',
+                rows: 2
+            } 
+        }
+        768: {
+            slidesPerView: 2,
+            grid: {
+                fill: 'row',
+                rows: 2
+            } 
+        }
+    }
+})
